e7071fa555c094e4cd1ad2a56782048517ac8237
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2021-03-22 10:08:40 -0300

    Changed the Hello font, fixed typist delay.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,7 +5,6 @@ import Typist from 'react-typist';
 import TypistLoop from 'react-typist-loop';
 
 import { BackgroundContainer, ContentContainer, OverlayContainer } from '../Components/Page';
-import { Card } from '../Components/Card';
 import DiscordActivity from '../Components/DiscordActivity';
 import { Title } from '../Components/Title';
 import { Paragraph } from '../Components/Paragraph';
@@ -44,32 +43,30 @@ export const Home: React.FC = () => {
             <ContentContainer>
 				<Section display="flex" direction="column" justify="center" align="center">
 					<MainTitleContainer>
-						<Paragraph style={{ fontFamily: 'Inter', fontSize: '1.5vw', fontWeight: 100 }}>
+						<Paragraph style={{ fontSize: '1.5vw', fontWeight: 100 }}>
 							Hello, 👋
 						</Paragraph>
 						<Title style={{ fontSize: '5vw' }}>
 							I'm Gabriel Spalato
 						</Title>
 						<TypewritterParagraph>
-							{/* This keeps the spacing. */}
 							<TypistLoop interval={3000}>
-    						{[
-      							'self-taught developer.',
-      							'C# enthusiast.',
-      							'graphic design apprentice.',
-								'diehard discord user.',
-								'y u bully me?',
-    						].map(text =>
-								<Typist>
-									<span>{text}</span>
-									<Typist.Delay ms={text.length * 100} />
-									<Typist.Backspace count={text.length} delay={text.length * 20} />
-								</Typist>
-							)}
+    							{[
+      								'self-taught developer.',
+      								'C# enthusiast.',
+      								'graphic design apprentice.',
+									'diehard discord user.',
+									'y u bully me?',
+    							].map(text =>
+									<Typist>
+										<span>{text}</span>
+										{/*<Typist.Delay ms={text.length * 100} />*/}
+										<Typist.Backspace count={text.length} delay={text.length * 200} />
+									</Typist>
+								)}
 							</TypistLoop>
 						</TypewritterParagraph>
 					</MainTitleContainer>
-					{/* presenceActive ? 'block' : 'none' */}
 					<DiscordPresence style={{ display: 'block' }} setActive={setPresenceActive}/>
 				</Section>
 				<Section>
