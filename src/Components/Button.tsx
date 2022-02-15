1657cb3ce60edf80dec1cca69fade8696e488617
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-15 20:29:42 -0300

    Buttons now work with links.

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -7,14 +7,32 @@ import { motion } from "framer-motion";
 interface IButtonProps {
     className?: string;
     text: string;
+    link?: string;
+    onClick?: () => void;
 }
 
-export const Button: React.FC<IButtonProps> = ({ className, text, children }) => {
+export const Button: React.FC<IButtonProps> = ({ className, text, link, onClick, children }) => {
+  if (link) {
+    return (
+      <motion.a
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95, filter: "brightness(75%)" }}
+        className={`bg-scheme-darkblue px-4 py-2 rounded-lg ${className ?? ""}`}
+        onClick={(() => onClick?.())}
+
+        target="_blank" href={link}
+      >
+        <h1 className="text-white font-display font-semibold text-base">{text}{children}</h1>
+      </motion.a>
+    );
+  }
+  
   return (
     <motion.button
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95, filter: "brightness(75%)" }}
         className={`bg-scheme-darkblue px-4 py-2 rounded-lg ${className ?? ""}`}
+        onClick={(() => onClick?.())}
     >
         <h1 className="text-white font-display font-semibold text-base">{text}{children}</h1>
     </motion.button>
