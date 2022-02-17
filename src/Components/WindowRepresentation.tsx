d6f5b4a647bdcc88f96d31189952925a8b3f75b5
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-17 20:21:17 -0300

    WR now adapts to window size, with changed colors.

--- src/Components/WindowRepresentation.tsx
+++ src/Components/WindowRepresentation.tsx
@@ -5,19 +5,19 @@ interface IWindowRepresentationProps { }
 
 export const WindowRepresentation: React.FC<IWindowRepresentationProps> = ({ children }) => {
     return (
-        <div className='relative row-start-1 col-start-6 xl:col-start-7 col-span-7 xl:col-span-6'>
-            <div className="-mx-4 sm:mx-0 min-h-[20rem] min-w-[35rem]">
+        <div className='relative row-start-1 col-start-6 xl:col-start-7 col-span-7 xl:col-span-6 z-10'>
+            <div className="-mx-4 sm:mx-0 md:min-h-[20rem] md:min-w-[35rem]">
                 <div className='relative overflow-hidden shadow-xl
                     flex bg-slate-800 h-[31.625rem] max-h-[60vh]
-                    sm:max-h-[none] sm:rounded-xl lg:h-[34.6875rem]
+                    sm:max-h-[none] rounded-xl lg:h-[34.6875rem]
                     xl:h-[31.625rem] bg-scheme-overlay-transparent backdrop-blur
-                    ring-1 ring-inset ring-white/10 !h-auto max-h-[none]'>
+                    ring-1 ring-inset ring-scheme-gray-500 !h-auto'>
                     <div className='relative w-full flex flex-col'>
-                        <div className="flex-none border-b border-white/10">
+                        <div className="flex-none border-b border-scheme-gray-500/90">
                             <div className="flex items-center h-8 space-x-1.5 px-3">
-                                <div className="w-2.5 h-2.5 bg-white/10 rounded-full"></div>
-                                <div className="w-2.5 h-2.5 bg-white/10 rounded-full"></div>
-                                <div className="w-2.5 h-2.5 bg-white/10 rounded-full"></div>
+                                <div className="w-2.5 h-2.5 bg-scheme-gray-500 rounded-full"></div>
+                                <div className="w-2.5 h-2.5 bg-scheme-gray-500 rounded-full"></div>
+                                <div className="w-2.5 h-2.5 bg-scheme-gray-500 rounded-full"></div>
                             </div>
                         </div>
                         <div className='relative min-h-max flex-auto flex flex-col'>
