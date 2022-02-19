b46b6037c267b73eddf99f778acd72707502b6ce
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-18 21:56:48 -0300

    Aligned the timeline entry pin, changed title size.

--- src/Components/TimelineEntry.tsx
+++ src/Components/TimelineEntry.tsx
@@ -12,10 +12,10 @@ export const TimelineEntry: React.FC<ITimelineEntryProps> = ({ date, title, desc
     return (
         <li className={last ? "ml-4" : "mb-10 ml-4"}>
             <div className="absolute h-[0.85rem] w-[0.85rem] rounded-full
-                -left-1.5 bg-scheme-gray-500 border-2 border-scheme-background"
+                -left-[7px] bg-scheme-gray-500 border-2 border-scheme-background"
             />
             <time className="mb-1 text-sm font-light leading-none text-scheme-offwhite/50">{date}</time>
-            <h3 className="text-lg font-semibold text-white/90">{title}</h3>
+            <h1 className="text-[1.35rem] font-semibold text-white/90">{title}</h1>
             <p className="mb-4 text-base font-normal text-scheme-offwhite/75 text-wrap max-w-sm">{description}</p>
             {children}
         </li>
