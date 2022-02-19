1ce2db29de9c455dca1f37ddaaf0db2838e18223
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-18 21:54:47 -0300

    Navbar is now common to all pages and is sticky.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-d
 import { Home } from './Pages/Home';
 import { Links } from './Pages/Links';
 
+import { DefaultNavbar } from './Partials/DefaultNavbar';
+import { NavbarItem } from './Partials/NavbarItem';
+
 import { Cursor } from './Components/Cursor';
 
 import './index.css';
@@ -15,6 +18,20 @@ const App: React.FC = () => {
     return (
         <main className="antialiased bg-scheme-background h-max">
             <AnimatePresence exitBeforeEnter>
+                <DefaultNavbar>
+					<NavbarItem link="https://www.instagram.com/gabriel.spalato/">
+						<i className="fa-brands fa-instagram fa-lg"></i>
+					</NavbarItem>
+					<NavbarItem link="https://github.com/gspalato">
+						<i className="fa-brands fa-github fa-lg"></i>
+					</NavbarItem>
+					<NavbarItem link="https://open.spotify.com/user/oubhvljhzyudfbxyx20opzxhq">
+						<i className="fa-brands fa-spotify fa-lg"></i>
+					</NavbarItem>
+					<NavbarItem link="https://www.linkedin.com/in/gabriel-marques-3aa183a8/">
+						<i className="fa-brands fa-linkedin fa-lg"></i>
+					</NavbarItem>
+				</DefaultNavbar>
                 <Router>
                     <Switch>
                         <Route exact path="/">
