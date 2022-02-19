e89b8ebcbf1e3154adbb1f6e64d119b4fcb5f425
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-18 22:30:59 -0300

    Replaced some sections with the new Section component.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -18,6 +18,7 @@ import MisfitsSwirl from "../Assets/img/misfits_swirl.png";
 import ArrowDown from "../Assets/img/arrow_down.svg";
 import WondersLogo from "../Assets/img/wonders.png";
 import DefaultIcon from "../Assets/img/icon.png";
+import { Section } from "../Components/Section";
 
 
 
@@ -39,6 +40,7 @@ export const Home: React.FC = () => {
 	return (
 		<>
 			<div className="bg-transparent backdrop-blur">
+				{/* Custom (Landing) Section */}
 				<section className="relative flex items-center flex-col justify-center h-screen max-w-screen-xl mx-auto pl-5 pr-5">
 					<div className="flex items-center flex-col md:flex-row justify-between">
 						<img src={ProfilePicture} draggable="false" className="animate-grow h-[22rem] w-auto rounded-full saturate-150 blur" />
@@ -57,37 +59,37 @@ export const Home: React.FC = () => {
 					</div>
 					<ArrowDown className="h-8 w-auto animate-bounce bottom-6 absolute mb-4 invert opacity-75" />
 				</section>
-				<section className="relative flex flex-col justify-center h-screen mx-auto">
-					<img src={MisfitsSwirl} className="absolute h-full w-full brightness-50" draggable="false" />
-					<div className="flex justify-center bg-scheme-contrast-transparent backdrop-blur-2xl h-full w-full pl-5 pr-5">
-						<div className="flex flex-col items-center justify-center max-w-screen-xl h-full w-full">
-							<h1 className="font-display font-bold text-scheme-offwhite text-5xl py-20">Projects</h1>
-							<CardView className="m-auto !w-min">
-								<Card image={WondersLogo} title="Wonders" description="A rainmeter-like widget platform powered by Electron!">
-									<Button text="Repository" link="https://github.com/wondersorg/wonders">
-										<i className="devicon-github-original my-auto leading-none text-lg text-scheme-offwhite pl-[0.4rem]"></i>
-									</Button>
-								</Card>
-								<Card image={DefaultIcon} title="Portfolio" description="My portfolio website, bootstrapped with Reactions.">
-									<Button text="Repository" link="https://github.com/gspalato/portfolio">
-										<i className="devicon-github-original my-auto leading-none text-lg text-scheme-offwhite pl-[0.4rem]"></i>
-									</Button>
-								</Card>
-								<Card image={DefaultIcon} title="Reactions" description="A boilerplate for React projects using Webpack 5 and Typescript; preconfigured with TailwindCSS.">
-									<Button text="Repository" link="https://github.com/gspalato/reactions">
-										<i className="devicon-github-original my-auto leading-none text-lg text-scheme-offwhite pl-[0.4rem]"></i>
-									</Button>
-								</Card>
-							</CardView>
-						</div>
+
+				<Section
+				title="Projects"
+				background={
+					<img src={MisfitsSwirl} className="h-full w-full brightness-50" draggable="false" />
+				}
+				blur overlayColor>
+					<div className="flex h-full w-full items-center justify-center">
+						<CardView className="m-auto !w-min">
+							<Card image={WondersLogo} title="Wonders" description="A rainmeter-like widget platform powered by Electron!">
+								<Button text="Repository" link="https://github.com/wondersorg/wonders">
+									<i className="devicon-github-original my-auto leading-none text-lg text-scheme-offwhite pl-[0.4rem]"></i>
+								</Button>
+							</Card>
+							<Card image={DefaultIcon} title="Portfolio" description="My portfolio website, bootstrapped with Reactions.">
+								<Button text="Repository" link="https://github.com/gspalato/portfolio">
+									<i className="devicon-github-original my-auto leading-none text-lg text-scheme-offwhite pl-[0.4rem]"></i>
+								</Button>
+							</Card>
+							<Card image={DefaultIcon} title="Reactions" description="A boilerplate for React projects using Webpack 5 and Typescript; preconfigured with TailwindCSS.">
+								<Button text="Repository" link="https://github.com/gspalato/reactions">
+									<i className="devicon-github-original my-auto leading-none text-lg text-scheme-offwhite pl-[0.4rem]"></i>
+								</Button>
+							</Card>
+						</CardView>
 					</div>
-				</section>
-				<section className="relative flex items-center flex-col justify-center h-max md:h-screen max-w-screen-xl mx-auto pl-5 pr-5">
-					<div className="flex items-center flex-col md:flex-row flex-1 w-full">
-						<div className="flex flex-col items-center justify-center h-full w-full flex-1">
-							<h1 className="font-display font-bold text-scheme-offwhite text-5xl py-20">Experience</h1>
-							<div className="flex flex-col md:flex-row flex-1 h-auto w-full">
-								<div className="flex flex-auto h-auto w-auto justify-center items-center">
+				</Section>
+
+				<Section title="Experience">
+					<div className="flex flex-col md:flex-row flex-1 h-auto w-full">
+						<div className="flex flex-auto h-auto w-auto justify-center items-center">
 									<Timeline>
 										<TimelineEntry 
 											date="January 2011 - December 2022"
@@ -101,8 +103,8 @@ export const Home: React.FC = () => {
 											last
 										/>
 									</Timeline>
-								</div>
-								<div className="flex flex-auto pt-10 md:pt-0 h-auto w-auto justify-center items-center">
+						</div>
+						<div className="flex flex-auto pt-10 md:pt-0 h-auto w-auto justify-center items-center">
 									<WindowRepresentation className="mb-6 md:mb-0">
 										<div className="p-6 grid grid-cols-5 gap-6 auto-rows-fr text-5xl text-scheme-offwhite">
 											<GridLogo>devicon-javascript-plain</GridLogo>
@@ -121,13 +123,13 @@ export const Home: React.FC = () => {
 											<GridLogo>devicon-mongodb-plain</GridLogo>
 										</div>
 									</WindowRepresentation>
-								</div>
-							</div>
 						</div>
 					</div>
-				</section>
+				</Section>
 			</div>
+
 			<DefaultFooter/>
+			
 			<div className="hidden md:block fixed left-4 bottom-4">
 				<DiscordPresence
 					setActive={setPresenceActive}
