ff575d20104f14bcc108ad8e3a13e093fc4798aa
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-18 21:57:29 -0300

    Reformatted card, fixed description positioning.

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -4,7 +4,7 @@ import React from "react";
 let Container = `flex flex-col justify-center items-center
 backdrop-blur-md bg-scheme-overlay-transparent
 border border-solid border-scheme-border/50
-p-4 left-4 rounded-xl min-h-[20rem] min-w-[15rem]`;
+p-6 left-4 rounded-xl min-h-[20rem] w-[15rem]`;
 
 // Component
 interface ICardProps {
@@ -16,9 +16,18 @@ interface ICardProps {
 export const Card: React.FC<ICardProps> = ({ image, title, description, children }) => {
     return (
         <div className={Container}>
-            <img src={image} className="rounded-full h-24 w-24 mb-6"/>
-            <h1 className="text-scheme-offwhite font-display text-2xl font-bold mb-4 active:brightness-75 transition-all">{title}</h1>
-            <p className="text-scheme-offwhite font-body text-base mb-6 break-words">{description}</p>
+            <img src={image} className="rounded-full h-24 w-24"/>
+            <h1 className="text-scheme-offwhite font-display text-2xl
+            font-bold my-4 active:brightness-75
+            transition-all">
+                {title}
+            </h1>
+            <div className="flex justify-center items-center flex-1 w-full">
+                <p className="text-scheme-offwhite
+                font-body text-base text-center mb-6 break-words">
+                    {description}
+                </p>
+            </div>
             {children}
         </div>
     );
