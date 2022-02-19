e3a63c631709f8c4cf070d2b74b920f47ae2789b
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-18 22:36:34 -0300

    Reformatted Section, added noMaxWidth prop.

--- src/Components/Section.tsx
+++ src/Components/Section.tsx
@@ -4,23 +4,26 @@ interface ISectionProps {
 	background?: React.ReactNode;
 	blur?: boolean;
 	className?: string;
+	noMaxWidth?: boolean;
 	overlayColor?: boolean;
 	title?: string | React.ReactNode;
 }
 
-export const Section: React.FC<ISectionProps> = ({ background, blur, className, overlayColor, title, children }) => {
+export const Section: React.FC<ISectionProps> = (props) => {
   return (
-    <section className={`relative flex flex-col justify-center h-screen mx-auto ${className ?? ""}`}>
+    <section className="relative flex flex-col justify-center h-screen mx-auto">
 			<div className="absolute h-full w-full">
-				{/* Background */}
-				{background}
+				{props.background}
 			</div>
 			<div className={`flex justify-center h-full w-full pl-5 pr-5
-			${blur ? "backdrop-blur-2xl" : ""} ${overlayColor ? "bg-scheme-contrast-transparent" : ""}`}>
-				<div className="flex flex-col items-center justify-center max-w-screen-xl h-full w-full">
-					<h1 className="font-display font-bold text-scheme-offwhite text-5xl py-24">{title}</h1>
-					<div className="flex-1 h-auto w-full">
-						{children}
+			${props.blur ? "backdrop-blur-2xl" : ""} ${props.overlayColor ? "bg-scheme-contrast-transparent" : ""}`}>
+				<div className={`flex flex-col items-center justify-center
+				h-full w-full ${!props.noMaxWidth ? "max-w-screen-xl" : ""}`}>
+					<h1 className="font-display font-bold text-scheme-offwhite text-5xl py-24">{props.title}</h1>
+					<div className={`flex-1 h-auto w-full ${props.className ?? ""}`}>
+						{/* This div should act just like a normal one. */}
+						{/* Styles applied to it shouldn't break. */}
+						{props.children}
 					</div>
 				</div>
 			</div>
