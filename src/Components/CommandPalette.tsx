dd074941dda82dd991f0fdfb2b2ec40ffe282b01
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-20 15:09:15 -0300

    Created a CommandPalette prototype.

---
+++ src/Components/CommandPalette.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Input } from './Input';
+
+export const CommandPalette: React.FC = () => {
+    return (
+        <div className="hidden fixed md:flex items-center justify-center
+        top-0 left-0 h-screen w-screen z-[100]
+        bg-[#000a]">
+            <div className="rounded-3xl bg-gray-300/75
+            flex flex-col m-auto h-1/2 w-1/2 z-[101] border
+            border-border backdrop-blur-lg">
+                <Input
+                className="!border-b !border-b-border pl-6"
+                placeholder="Type something to search."/>
+                <div className="">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
