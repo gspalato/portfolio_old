cd1a099f34b2e2eddc7235f56e87a5ccd879d2ad
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-24 19:30:56 -0300

    Added a transparent prop to Button.

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -9,15 +9,16 @@ interface IButtonProps {
     text: string;
     link?: string;
     onClick?: () => void;
+    transparent?: boolean;
 }
 
-export const Button: React.FC<IButtonProps> = ({ className, text, link, onClick, children }) => {
+export const Button: React.FC<IButtonProps> = ({ className, text, link, onClick, transparent, children }) => {
   if (link) {
     return (
       <motion.a
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95, filter: "brightness(75%)" }}
-        className={`bg-darkblue px-4 py-2 rounded-lg ${className ?? ""}`}
+        className={`${transparent ? "bg-transparent" : "bg-darkblue"} px-4 py-2 rounded-lg ${className ?? ""}`}
         onClick={(() => onClick?.())}
 
         target="_blank" href={link}
@@ -31,7 +32,7 @@ export const Button: React.FC<IButtonProps> = ({ className, text, link, onClick,
     <motion.button
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95, filter: "brightness(75%)" }}
-        className={`bg-darkblue px-4 py-2 rounded-lg ${className ?? ""}`}
+        className={`${transparent ? "bg-transparent" : "bg-darkblue"} px-4 py-2 rounded-lg ${className ?? ""}`}
         onClick={(() => onClick?.())}
     >
         <h1 className="flex items-center align-middle text-white font-display font-medium text-base">{text}{children}</h1>
