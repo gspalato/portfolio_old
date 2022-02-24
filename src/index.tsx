43b40825614fd8fd01e2c8a220b4707499095d05
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-24 19:30:44 -0300

    Fixed imports.

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,11 @@ import { AnimatePresence } from 'framer-motion';
 
 import App from './App';
 
-import { DefaultNavbar as Navbar } from './Partials/DefaultNavbar';
+import { Navbar } from './Partials/Navbar';
 import { NavbarItem } from "./Partials/NavbarItem";
 
 import { Cursor } from './Components/Cursor';
-import { DiscordPresence } from './Components/DiscordPresence';
+import { Doing } from './Components/Doing';
 
 import './index.css';
 
@@ -18,11 +18,10 @@ ReactDOM.render(
 	<main className="antialiased h-max
 selection:bg-darkblue selection:text-offwhite/90">
     <Router>
-			<Navbar/>
 			<App />
 		</Router>
 		<div className="hidden md:block fixed left-4 bottom-4">
-			<DiscordPresence setActive={() => {}} />
+			<Doing setActive={() => {}} />
 		</div>
 		<Cursor/>
 	</main>,
