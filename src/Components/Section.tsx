16c03f8cce06e70fb3bd0931026177eef31faf83
Gabriel <3950465+gspalato@users.noreply.github.com>
2022-02-26 14:06:28 -0300

    Added a noTitle option to Section.

--- src/Components/Section.tsx
+++ src/Components/Section.tsx
@@ -5,13 +5,14 @@ interface ISectionProps {
 	blur?: boolean;
 	className?: string;
 	noMaxWidth?: boolean;
+	noTitle?: boolean;
 	overlayColor?: boolean;
 	title?: string | React.ReactNode;
 }
 
 export const Section: React.FC<ISectionProps> = (props) => {
   return (
-    <section className="relative flex flex-col justify-center h-screen mx-auto">
+    <section className="relative flex flex-col justify-center h-screen w-full">
 			<div className="absolute h-full w-full">
 				{props.background}
 			</div>
@@ -19,7 +20,7 @@ export const Section: React.FC<ISectionProps> = (props) => {
 			${props.blur ? "backdrop-blur-2xl" : ""} ${props.overlayColor ? "bg-contrast-transparent" : ""}`}>
 				<div className={`flex flex-col items-center justify-center
 				h-full w-full ${!props.noMaxWidth ? "max-w-screen-xl" : ""}`}>
-					<h1 className="font-display font-bold text-offwhite text-5xl py-24">{props.title}</h1>
+					{ props.noTitle ? null : <h1 className="font-display font-bold text-offwhite text-4xl py-32">{props.title}</h1> }
 					<div className={`flex-1 h-auto w-full ${props.className ?? ""}`}>
 						{/* This div should act just like a normal one. */}
 						{/* Styles applied to it shouldn't break. */}
