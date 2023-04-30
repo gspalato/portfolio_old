6aaa21db6353c92955817857652ead58796d5ed2
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2023-04-29 22:29:37 -0300

    Created a placeholder "About" page.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+    return (
+        <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1rem' }}>
+            <h1 style={{ color: '#fff', fontSize: '5rem', letterSpacing: '-0.05rem' }}>About</h1>
+        </div>
+    );
+}
