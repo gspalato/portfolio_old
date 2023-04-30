d059457ea1ba9b1a5c3c17713e0745c82ee28eb9
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2023-04-30 17:23:14 -0300

    Fixed Home page SASS module import.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import Background from '@/components/Background';
 import Page from '@/components/Page';
 
-import Styles from './page.module.sass';
+import Styles from './home.module.sass';
 
 export default function Home() {
   return (
