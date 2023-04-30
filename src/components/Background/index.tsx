c47c54424a8bc4b95747bf57d1ff30f27b2681c1
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2023-04-30 17:22:39 -0300

    Changed Background back to a motion component.

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -8,67 +8,57 @@ import { v4 as uuid } from 'uuid';
 import Styles from './background.module.sass';
 
 export enum ImageFit {
-    cover = "object-cover",
-    contain = "object-contain",
-    fill = "object-fill",
-    none = "object-none",
-    scaleDown = "object-scale-down"
+	cover = "object-cover",
+	contain = "object-contain",
+	fill = "object-fill",
+	none = "object-none",
+	scaleDown = "object-scale-down"
 }
 
 interface IBackgroundProps extends React.PropsWithChildren {
-    children?: any;
-    className?: string;
-    fit?: ImageFit;
-    darken?: boolean;
-    src: any;
+	children?: any;
+	className?: string;
+	fit?: ImageFit;
+	darken?: boolean;
+	src: any;
 }
 
 const Background: React.FC<IBackgroundProps> = (props) => {
-    const {
-        children,
-        className,
-        fit,
-        darken,
-        src
-    } = props;
+	const {
+		children,
+		className,
+		fit,
+		darken,
+		src
+	} = props;
 
-    const [domReady, setDomReady] = useState(false);
+	const [domReady, setDomReady] = useState(false);
 
-    useEffect(() => {
-        setDomReady(true);
-        return () => setDomReady(false);;
-    });
+	useEffect(() => {
+		setDomReady(true);
+		return () => setDomReady(false);
+	});
 
-    let objectFit: string = fit?.toString() ?? 'cover';
+	const objectFit: string = fit?.toString() ?? 'cover';
 
-    const nodes = (
-        <motion.div
-            key={uuid()}
-            
-            style={{ position: 'absolute', height: '100%', width: '100%' }}
+	const nodes = (
+		<motion.div
+			key={uuid()}
+			
+			style={{ position: 'absolute', height: '100%', width: '100%' }}
 
-            initial={{ opacity: 0 }}
-		    animate={{ opacity: 1, filter: "brightness(0.5)" }}
-		    exit={{ opacity: 0 }}
-		    transition={{ delay:.5, duration: 0.75 }}
-        >
-            {children}
-            {darken && <div className={Styles.overlay} key="dark-overlay" /> }
-            {
-                React.createElement(motion.img, {
-                    className: className ?? "",
-		    	    src: src,
-                    alt: "",
-                    style: {
-                        position: 'absolute',
-                        filter: "blur(10px)",
-                    },
-                })
-            }
-        </motion.div>
-    );
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1, filter: "brightness(0.5)" }}
+			exit={{ opacity: 0 }}
+			transition={{ delay:.5, duration: 0.75 }}
+		>
+			{children}
+			{darken && <div className={Styles.overlay} key="dark-overlay" /> }
+			<motion.img className={className} src={src} alt="" style={{ position: 'absolute' }} />
+		</motion.div>
+	);
 
-    return domReady ? ReactDOM.createPortal(nodes, document.getElementById("background")!) : null;
+	return domReady ? ReactDOM.createPortal(nodes, document.getElementById("background")!) : null;
 }
 
 export default Background;
