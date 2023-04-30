43c95311228ac43dce23678844d0d2f29f28eceb
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2023-04-30 17:20:17 -0300

    Fixed some typing warnings.

--- src/components/Cursor/index.tsx
+++ src/components/Cursor/index.tsx
@@ -1,12 +1,10 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { motion, useMotionValue, useSpring } from 'framer-motion';
 
 import Styles from './cursor.module.sass';
 
-const Cursor: React.FC = (props) => {
-    const cursorX = useMotionValue(-100);
+const Cursor: React.FC = () => {
+	const cursorX = useMotionValue(-100);
 	const cursorY = useMotionValue(-100);
 
 	const auraX = useMotionValue(-100);
@@ -15,51 +13,51 @@ const Cursor: React.FC = (props) => {
 	const auraXSpring = useSpring(auraX, { damping: 50, stiffness: 500 });
 	const auraYSpring = useSpring(auraY, { damping: 50, stiffness: 500 });
 
-	let isTouchDevice: boolean = false;
+	let isTouchDevice = false;
 	useEffect(() => {
 		isTouchDevice = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0);
 	})
 
 	const [beingClicked, setBeingClicked] = useState(false);
 
-    useEffect(() => {
-        const moveCursor = (e: any) => {
-            cursorX.set(e.clientX);
-            cursorY.set(e.clientY);
+	useEffect(() => {
+		const moveCursor = (e: MouseEvent) => {
+			cursorX.set(e.clientX);
+			cursorY.set(e.clientY);
 
 			auraX.set(e.clientX - 10);
 			auraY.set(e.clientY - 10);
-        };
+		};
 
-		const mouseDownCursor = (e: any) => {
+		const mouseDownCursor = () => {
 			setBeingClicked(true);
 		}
 
-		const mouseUpCursor = (e: any) => {
+		const mouseUpCursor = () => {
 			setBeingClicked(false);
 		}
 
-        window.addEventListener("mousemove", moveCursor);
+		window.addEventListener("mousemove", moveCursor);
 		window.addEventListener("mousedown", mouseDownCursor);
 		window.addEventListener("mouseup", mouseUpCursor);
 
-        return () => {
-            window.removeEventListener("mousemove", moveCursor);
-	  	    window.removeEventListener("mousedown", mouseDownCursor);
-	  	    window.removeEventListener("mouseup", mouseUpCursor);
-        };
-    } , []);
+		return () => {
+			window.removeEventListener("mousemove", moveCursor);
+			window.removeEventListener("mousedown", mouseDownCursor);
+			window.removeEventListener("mouseup", mouseUpCursor);
+		};
+	});
 
 	return (
 		<>
-		    <motion.div
+			<motion.div
 				className={Styles.cursorInner}
-    	        style={{
-				    display: isTouchDevice ? 'none': 'block',
-    	            translateX: cursorX,
-    	            translateY: cursorY,
-    	        }}
-    	    />
+				style={{
+					display: isTouchDevice ? 'none': 'block',
+					translateX: cursorX,
+					translateY: cursorY,
+				}}
+			/>
 			<motion.div
 				className={Styles.cursorOuter}
 				style={{
