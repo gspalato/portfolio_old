100e356181d784adf950670b60f2699fe54ce37a
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2023-04-29 22:31:25 -0300

    Removed unused Stitches file import.

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { styled } from '@/styles';
 import { motion, useMotionValue } from 'framer-motion';
 import Image from 'next/image';
 
