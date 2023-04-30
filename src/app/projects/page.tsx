38129b62a21194dd28844d07cad80a6310c81995
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2023-04-29 22:17:31 -0300

    Changed test text color.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 export default function Projects() {
     return (
         <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '1rem' }}>
-            <h1 style={{ fontSize: '5rem', letterSpacing: '-0.05rem' }}>Projects</h1>
+            <h1 style={{ color: '#fff', fontSize: '5rem', letterSpacing: '-0.05rem' }}>Projects</h1>
         </div>
     );
 }
