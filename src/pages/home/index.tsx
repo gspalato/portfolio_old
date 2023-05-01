3a548471e8c15da6739b56085816f31d926cce30
Gabriel Spalato Marques <3950465+gspalato@users.noreply.github.com>
2023-05-01 13:22:24 -0300

    Created a new homepage hero section concept.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import useFitText from 'use-fit-text';
 
 import Background from '@/components/Background';
 import Page from '@/components/Page';
@@ -6,9 +7,18 @@ import Page from '@/components/Page';
 import Styles from './home.module.sass';
 
 export default function Home() {
-  return (
-    <Page>
-        <motion.div className={Styles.bubble}/>
-    </Page>
-  )
+  	return (
+    	<Page>
+			<Background src="">
+				{/* <motion.div className={Styles.bubble} /> */}
+			</Background>
+			<section className={Styles.content}>
+				<div className={Styles.textContainer}>
+					<h1 className={Styles.heroText}>full-stack</h1>
+					<h1 className={Styles.heroText}>developer</h1>
+					<h1 className={Styles.heroText}>& designer</h1>
+				</div>
+			</section>
+    	</Page>
+  	)
 }
