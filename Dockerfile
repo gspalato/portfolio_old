2811afde469c8acfd804907f6e249b4a63d8e17a
gspalato <3950465+gspalato@users.noreply.github.com>
2023-06-29 16:57:21 -0300

    Updated Vite config and Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,11 @@ COPY ./Portfolio/package.json .
 RUN npm i
 RUN npm i serve -g
 
-# Copy source code and .env file.
+# Copy source code.
 COPY ./Portfolio .
+
+# Copy .env file containing the API URL.
+COPY ./.portfolio.env ./.env
 
 RUN npm run build
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	resolve: {
 		alias: {
 			'@': path.join(__dirname, 'src/'),
 		},
 	},
 	define: {
-		'process.env': process.env,
+		'process.env': { ...process.env, ...loadEnv(mode, process.cwd()) },
 	},
 	plugins: [react()],
 	server: {
@@ -21,4 +21,4 @@ export default defineConfig({
 		strictPort: true,
 		port: 8080,
 	},
-});
+}));
