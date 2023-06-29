9d910936bd80d9b04bb59344e0873cf934644819
gspalato <3950465+gspalato@users.noreply.github.com>
2023-06-29 16:04:41 -0300

    Added dotenv to load the .env file during runtime.

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 
+import App from './App.tsx';
+
 import Client from '@/lib/graphql/client';
 
-import App from './App.tsx';
+import 'dotenv/config';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
