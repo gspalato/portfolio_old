1a61bfabf67ab0fa9bb2a15c91b20826c375c505
gspalato <3950465+gspalato@users.noreply.github.com>
2023-06-30 15:18:47 -0300

    Removed unused imports.

--- src/components/Tabs/TabContent.tsx
+++ src/components/Tabs/TabContent.tsx
@@ -1,8 +1,5 @@
 import { AnimatePresence } from 'framer-motion';
 import React from 'react';
-import { v4 as uuid } from 'uuid';
-
-import { useTabs } from './Tabs';
 
 interface ITabContentProps extends React.PropsWithChildren {
 	animate?: boolean;
@@ -10,7 +7,6 @@ interface ITabContentProps extends React.PropsWithChildren {
 
 const Component: React.FC<ITabContentProps> = (props) => {
 	const { animate = true, children } = props;
-	const { currentTab } = useTabs();
 
 	return animate ? (
 		<AnimatePresence mode='wait'>{children}</AnimatePresence>
