470e77275fed610896250d6acac511f6c5a3c581
gspalato <3950465+gspalato@users.noreply.github.com>
2023-06-29 22:15:25 -0300

    Added many new colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,15 +18,30 @@ module.exports = {
 		extend: {
 			colors: {
 				accents: {
-					1: '#111',
-					2: '#333',
-					3: '#555',
-					4: '#777',
-					5: '#999',
-					6: '#BBB',
-					7: '#DDD',
-					8: '#FFF',
+					0: 'rgb(6, 6, 7)',
+					1: 'rgb(12, 12, 14)',
+					2: 'rgb(28, 28, 30)',
+					3: 'rgb(44, 44, 46)',
+					4: 'rgb(58, 58, 60)',
+					5: 'rgb(72, 72, 74)',
+					6: 'rgb(99, 99, 102)',
+					7: 'rgb(142, 142, 147)',
 				},
+				overlays: {
+					0: '#ffffff09',
+					1: '#ffffff0a',
+					2: '#ffffff0c',
+					3: '#ffffff0e',
+					4: '#ffffff0f',
+					5: '#ffffff11',
+					6: '#ffffff22',
+					7: '#ffffff33',
+					8: '#ffffff44',
+				},
+
+				white: 'fafafa',
+
+				ring: '#ffffff11',
 
 				background: '#09090b',
 				foreground: '#fafafa',
@@ -46,8 +61,9 @@ module.exports = {
 				accentForeground: '#fafafa',
 				destructive: '#7f1d1d',
 				destructiveForeground: '#fef2f2',
-				ring: '#27272a',
-				overlay: '#ffffff09',
+			},
+			backgroundImage: {
+				shadowy: 'linear-gradient(180deg, #fafafa, #adadad)',
 			},
 			boxShadow: {},
 			borderRadius: {},
