45f33a7b4e3f5b637a056b8ec197404caf5a2f1a
gspalato <3950465+gspalato@users.noreply.github.com>
2023-06-29 22:17:49 -0300

    Input is now styled entirely with Tailwind.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
 
 import Styles from './input.module.sass';
+import classes from '@/lib/classes';
 
 interface IInputProps {
-    className?: string;
-    placeholder?: string;
-    [key: string]: any;
+	className?: string;
+	placeholder?: string;
+	[key: string]: any;
 }
 
 const Component: React.FC<IInputProps> = (props) => {
-    const {
-        className,
-        placeholder,
-        ...moreProps
-    } = props
+	const { className, placeholder, ...moreProps } = props;
 
-    const classNames = [Styles.input, className].join(' ');
+	const classNames = classes(
+		`
+        bg-accents-1
+        h-12
+        w-[75%]
+        border
+        border-neutral-border
+        rounded-md
+        p-4
+        mb-8
+        transition-all
+        text-foreground
+        font-display
+        tracking-tight
+        outline-transparent
 
-    return (
-        <input className={classNames} placeholder={placeholder} {...moreProps} />
-    );
-}
+        duration-200
+        ease-in-out
+
+        hover:border
+        hover:border-[#ffffff33]
+        hover:outline-none
+
+        focus:border-blue
+        focus:outline-none
+
+        placeholder-[#ffffff33]
+        `,
+		className
+	);
+
+	return (
+		<input
+			className={classNames}
+			placeholder={placeholder}
+			{...moreProps}
+		/>
+	);
+};
 
 export default Component;
