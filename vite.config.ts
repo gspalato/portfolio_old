080566f3309bd7cb6392dda659b6d5a2ecb05450
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-06 16:23:36 -0300

    Updated Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+	build: {
+		rollupOptions: {
+			external: ['class-variance-authority'],
+		},
+	},
 	resolve: {
 		alias: {
 			'@': path.join(__dirname, 'src/'),
