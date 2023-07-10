3a8723ae020120ccf8f20a7e829dd4fbbb94193f
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-10 00:05:11 -0300

    Tooltip now has CSS options to enable GPU processing. (Didn't really help with the blurriness.)

--- src/components/Chart/Tooltip.tsx
+++ src/components/Chart/Tooltip.tsx
@@ -6,10 +6,15 @@ const Component: React.FC<SliceTooltipProps> = (props) => {
 
 	return (
 		<motion.div
-			className='glass optimize rounded-[5px] border border-accents-3 !bg-[#0007] shadow-md shadow-black/25'
+			className='glass optimize rounded-[5px] border border-[#ffffff11] !bg-[#0008] shadow-md shadow-black/25'
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			transition={{ duration: 0.1, delay: 0.01 }}
+			style={{
+				backfaceVisibility: 'hidden',
+				willChange: 'transform',
+				fontSmooth: 'subpixel-antialiased',
+			}}
 		>
 			<h1 className='w-full border-b border-[#ffffff11] py-2 text-center'>
 				<strong>{slice.points[0].data.x.toString()}</strong>
