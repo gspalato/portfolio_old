4e47dcf8d07e17352a9d30884f9e0366740c8805
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-10 00:06:07 -0300

    Now the CardHeader also has a gradient border.

--- src/components/Card/CardHeader.tsx
+++ src/components/Card/CardHeader.tsx
@@ -1,5 +1,7 @@
 import classes from '@lib/classes';
 
+import Styles from './card.module.sass';
+
 interface ICardHeaderProps extends React.PropsWithChildren {
 	className?: string;
 	separate?: boolean;
@@ -9,8 +11,11 @@ const Component: React.FC<ICardHeaderProps> = (props) => {
 	const { children, className, separate = true, ...rest } = props;
 
 	const classNames = classes(
+		Styles.gradientBorder,
 		'flex items-center justify-between px-6 py-5 md:min-h-[80px]',
-		separate ? 'border-b border-ring' : '',
+		separate
+			? 'rounded-t-lg !border-l-0 !border-r-0 !border-t-0 border-b'
+			: '!border-none',
 		className
 	);
 
