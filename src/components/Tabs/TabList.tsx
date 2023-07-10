74d5b69c4dd6504be37f4bdd86f8230ae75e0c87
gspalato <3950465+gspalato@users.noreply.github.com>
2023-07-10 00:07:44 -0300

    TabList now also uses gradient borders.

--- src/components/Tabs/TabList.tsx
+++ src/components/Tabs/TabList.tsx
@@ -1,5 +1,7 @@
 import classes from '@lib/classes';
 
+import Styles from './TabList.module.sass';
+
 interface ITabListProps extends React.PropsWithChildren {
 	className?: string;
 	width?: string;
@@ -9,7 +11,8 @@ const Component: React.FC<ITabListProps> = (props) => {
 	const { className, children, width } = props;
 
 	const classNames = classes(
-		'flex gap-2 rounded-md border border-ring bg-transparent p-1 shadow-black/50 drop-shadow',
+		Styles.tabList,
+		'flex gap-2 rounded-md bg-transparent p-1 shadow-black/50 drop-shadow',
 		className
 	);
 
