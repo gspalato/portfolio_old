37e7c986fc601f651de0d84179dca51447fe7929
Gabriel <3950465+gspalato@users.noreply.github.com>
2023-10-30 15:31:40 -0300

    Fixed the Project page's loading indicator.

--- src/app/Projects/index.tsx
+++ src/app/Projects/index.tsx
@@ -95,49 +95,10 @@ const Component = () => {
 				<h1 className='w-full py-4 pb-12 text-center font-mono text-3xl text-overlays-9 md:py-5'>
 					{'<projects>'}
 				</h1>
-				<AnimatePresence mode='wait'>
-					{(loading || loadingAssets) && (
-						<motion.div
-							key='loading'
-							initial={{ opacity: 0 }}
-							animate={{ opacity: 1 }}
-							exit={{ opacity: 0 }}
-							transition={{ duration: 0.1 }}
-							className='flex h-[22.5rem] w-full items-center justify-center'
-						>
-							<RotatingLines
-								strokeColor='grey'
-								animationDuration='1.5'
-								strokeWidth='4'
-								width='25'
-							/>
-						</motion.div>
-					)}
-				</AnimatePresence>
 				{!isPortrait && (
 					<div className='flex w-full flex-1 items-center justify-center px-2'>
-						<Carousel
-							className='h-[40rem] max-h-[75vmin] w-full max-w-[80%]'
-							data={projects}
-						>
-							{projects.map((project, i) => (
-								<ProjectBanner
-									project={project}
-									variants={ProjectBannerAnimationVariants}
-									initial='initial'
-									animate='animate'
-									custom={i}
-									key={project.name}
-									className='aspect-none !h-full !w-full'
-								/>
-							))}
-						</Carousel>
-					</div>
-				)}
-				{isPortrait && (
-					<motion.div className='top-0 grid flex-1 grid-cols-1 gap-8 px-8 pb-12 transition-all duration-200 md:flex md:flex-row md:flex-wrap md:place-content-center'>
 						<AnimatePresence mode='wait'>
-							{(loading || loadingAssets) && (
+							{loading || loadingAssets ? (
 								<motion.div
 									key='loading'
 									initial={{ opacity: 0 }}
@@ -153,18 +114,60 @@ const Component = () => {
 										width='25'
 									/>
 								</motion.div>
+							) : (
+								<Carousel
+									className='h-[40rem] max-h-[75vmin] w-full max-w-[80%]'
+									data={projects}
+								>
+									{projects.map((project, i) => (
+										<ProjectBanner
+											project={project}
+											variants={
+												ProjectBannerAnimationVariants
+											}
+											initial='initial'
+											animate='animate'
+											custom={i}
+											key={project.name}
+											className='aspect-none !h-full !w-full'
+										/>
+									))}
+								</Carousel>
 							)}
-
-							{projects.map((project, i) => (
-								<ProjectCard
-									project={project}
-									variants={ProjectCardAnimationVariants}
-									initial='initial'
-									animate='animate'
-									custom={i}
-									key={project.name}
-								/>
-							))}
+						</AnimatePresence>
+					</div>
+				)}
+				{isPortrait && (
+					<motion.div className='top-0 grid flex-1 grid-cols-1 gap-8 px-8 pb-12 transition-all duration-200 md:flex md:flex-row md:flex-wrap md:place-content-center'>
+						<AnimatePresence mode='wait'>
+							{loading || loadingAssets ? (
+								<motion.div
+									key='loading'
+									initial={{ opacity: 0 }}
+									animate={{ opacity: 1 }}
+									exit={{ opacity: 0 }}
+									transition={{ duration: 0.1 }}
+									className='flex h-[22.5rem] w-full items-center justify-center'
+								>
+									<RotatingLines
+										strokeColor='grey'
+										animationDuration='1.5'
+										strokeWidth='4'
+										width='25'
+									/>
+								</motion.div>
+							) : (
+								projects.map((project, i) => (
+									<ProjectCard
+										project={project}
+										variants={ProjectCardAnimationVariants}
+										initial='initial'
+										animate='animate'
+										custom={i}
+										key={project.name}
+									/>
+								))
+							)}
 						</AnimatePresence>
 					</motion.div>
 				)}
